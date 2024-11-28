56fc7c04be8e49910356e9fcccc0a6f59aec78cd
Umer Abubeker <umer.ali0986@gmail.com>
2024-11-28 08:50:59 -0600

    Changed app's docker and pom file

--- Docker-files/db/employee_management.sql
+++ Docker-files/db/employee_management.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `employee`
 --
-
+USE employee_management;
 DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
